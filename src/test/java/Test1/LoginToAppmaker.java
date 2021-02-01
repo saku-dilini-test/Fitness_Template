c41401cfa6158fbd022a6060fd71f1ae0854d47a
saku-dilini-test <sakudilini1993@gmail.com>
2021-02-01 17:22:03 +0530

    Add all my files73

--- src/test/java/Test1/LoginToAppmaker.java
+++ src/test/java/Test1/LoginToAppmaker.java
@@ -18,9 +18,9 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.apache.commons.io.FileUtils;
+
 import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
